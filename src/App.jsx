2213d3e5f2939d7793eb8e8341f217db6a6ee3b2
Daniel Aparisi <daniel.aparisi.lozano.com>
2024-04-13 12:29:00 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Slider from './components/Slider'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <main className="wrapper">
+        <section>
+          <h3>Craft Pizza Menu</h3>
+        </section>
+        <Slider></Slider>
+      </main>
+    </>
+    
+  );
+}
+
+export default App;
